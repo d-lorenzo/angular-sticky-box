9111c0eee586b44be9894eebe5f69674f177edfc
Daniel Lorenzo <daniel.lorenzoabalo@plexus.es>
2016-10-07 19:22:18 +0200

    Removed $timeout on scroll. Formatting.

--- src/angular-sticky-box.js
+++ src/angular-sticky-box.js
@@ -27,22 +27,22 @@ angular.module('angular-sticky-box', []).directive('stickyBox', ['$timeout', fun
 				{
 					top: scope.wrapBottom - viewHeight,
 					className: 'bottom',
-					style: [['top', (el.offsetHeight - scope.innerHeight)+'px']]
+					style: [['top', (el.offsetHeight - scope.innerHeight) + 'px']]
 				},
 				{
 					top: scope.wrapTop - scope.cfg.offset,
 					className: 'top',
 					style: [['top', 'auto']],
-					up:function(scope, el) {
+					up: function (scope, el) {
 						scope.pageYup = window.pageYOffset;
 						var bottom = parseInt(el.children[0].style.bottom);
-							if (scope.pageYdown - window.pageYOffset <= scope.innerHeight - viewHeight + scope.cfg.offset) {
-								el.children[0].style.bottom = (bottom + window.pageYOffset - scope.pageY )+'px';
-							} else {
-								el.children[0].style.bottom = (viewHeight - scope.innerHeight - scope.cfg.offset)+'px';
-							}
+						if (scope.pageYdown - window.pageYOffset <= scope.innerHeight - viewHeight + scope.cfg.offset) {
+							el.children[0].style.bottom = (bottom + window.pageYOffset - scope.pageY) + 'px';
+						} else {
+							el.children[0].style.bottom = (viewHeight - scope.innerHeight - scope.cfg.offset) + 'px';
+						}
 					},
-					down:function(scope, el) {
+					down: function (scope, el) {
 						scope.pageYdown = window.pageYOffset;
 						if (!scope.pageYup) {
 							scope.pageYup = scope.pageY;
@@ -53,31 +53,31 @@ angular.module('angular-sticky-box', []).directive('stickyBox', ['$timeout', fun
 							bottom = viewHeight - scope.innerHeight - scope.cfg.offset;
 						}
 						if (bottom < 0) {
-							el.children[0].style.bottom = (bottom + window.pageYOffset - scope.pageY )+'px';
+							el.children[0].style.bottom = (bottom + window.pageYOffset - scope.pageY) + 'px';
 						} else {
 							el.children[0].style.bottom = '0px';
 						}
 					}
 				}
 			];
-			el.children[0].className = scope.sizeClass+' '+scope.posClass;
+			el.children[0].className = scope.sizeClass + ' ' + scope.posClass;
 		} else {
 			scope.sizeClass = 'sticky-inner-small';
 			scope.breaks = [
 				{
 					top: scope.wrapBottom - scope.innerHeight - scope.cfg.offset,
 					className: 'bottom',
-					style: [['top', (el.offsetHeight - scope.innerHeight)+'px']]
+					style: [['top', (el.offsetHeight - scope.innerHeight) + 'px']]
 				},
 				{
 					top: scope.wrapTop - scope.cfg.offset,
 					className: 'top',
-					style: [['top', scope.cfg.offset+'px']]
+					style: [['top', scope.cfg.offset + 'px']]
 				}
 			];
-			el.children[0].className = scope.sizeClass+' '+scope.posClass;
+			el.children[0].className = scope.sizeClass + ' ' + scope.posClass;
 		}
-		el.children[0].style.width = (el.offsetWidth)+'px';
+		el.children[0].style.width = (el.offsetWidth) + 'px';
 
 		scroll(scope, el);
 	}
@@ -96,11 +96,11 @@ angular.module('angular-sticky-box', []).directive('stickyBox', ['$timeout', fun
 		}
 
 		var currentBreak = false;
-		for (var i=0; i<scope.breaks.length; i++) {
+		for (var i = 0; i < scope.breaks.length; i++) {
 			if (window.pageYOffset > scope.breaks[i].top) {
 				if (scope.posClass != scope.breaks[i].className) {
 					scope.posClass = scope.breaks[i].className;
-					for (var j=0; j<scope.breaks[i].style.length; j++) {
+					for (var j = 0; j < scope.breaks[i].style.length; j++) {
 						el.children[0].style[scope.breaks[i].style[j][0]] = scope.breaks[i].style[j][1];
 					}
 				}
@@ -124,16 +124,16 @@ angular.module('angular-sticky-box', []).directive('stickyBox', ['$timeout', fun
 		}
 		scope.pageY = window.pageYOffset;
 
-		el.children[0].className = scope.sizeClass+' '+scope.posClass;
+		el.children[0].className = scope.sizeClass + ' ' + scope.posClass;
 	}
 
 
 	return {
 		restrict: 'A',
-		scope:{
-			offset:'@stickyBoxOffset'
+		scope: {
+			offset: '@stickyBoxOffset'
 		},
-		link: function(scope, element) {
+		link: function (scope, element) {
 			function setupScope() {
 				setup(scope, el);
 			}
@@ -149,18 +149,16 @@ angular.module('angular-sticky-box', []).directive('stickyBox', ['$timeout', fun
 
 			scope.pageY = scope.pageYup = scope.pageYdown = window.pageYOffset;
 
-			angular.element(window).on('resize', function() {
-				$timeout(function() {
+			angular.element(window).on('resize', function () {
+				$timeout(function () {
 					setup(scope, el);
 				});
 			});
-			angular.element(window).on('scroll', function() {
-				$timeout(function() {
-					setup(scope, el);
-				});
+			angular.element(window).on('scroll', function () {
+				setup(scope, el);
 			});
 
-			scope.$on('stickyBoxUpdate', function() {
+			scope.$on('stickyBoxUpdate', function () {
 				$timeout(setupScope, 100);
 			});
 
